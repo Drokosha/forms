4aa70ec4013f0be184bca782a2b397cf0bfee567
Drokosha <drokok@mail.ru>
2021-08-26 22:40:36 +0300

    не получается

--- js/script.js
+++ js/script.js
@@ -3,43 +3,23 @@ window.onload = function() {
     document.forms.myform[1].onkeyup = onch;
     document.forms.myform[2].onkeyup = onch;
     document.forms.myform.onsubmit = chek;
+
+
 };
     function onch () {
-		if (document.forms.myform.firstname.value != "") {
-			document.getElementById('pop-up1').style.display = 'none';
-			document.forms.myform[0].classList.add("field_normal");
-			document.forms.myform[0].classList.remove("field_error");
+		if (this.value != "") {
+	        document.getElementById(this.name).style.display = 'none';
+			this.classList.add("field_normal");
+			this.classList.remove("field_error");
 		}
-			if (document.forms.myform.lastname.value != "") {
-					document.getElementById('pop-up2').style.display = 'none';
-			        document.forms.myform[1].classList.add("field_normal");
-				    document.forms.myform[1].classList.remove("field_error");
-			}
-				if (document.forms.myform.birthdate.value != "") {
-						document.getElementById('pop-up3').style.display = 'none';
-			        	document.forms.myform[2].classList.add("field_normal");
-						document.forms.myform[2].classList.remove("field_error");
-				}
 	}
 	    function chek() {
 		    valed = true;
-				if (document.forms.myform.firstname.value == "") {
-		            document.getElementById('pop-up1').style.display = 'flex';
-		            document.forms.myform[0].classList.remove("field_normal");
-		            document.forms.myform[0].classList.add("field_error");
+				if (this.value == "") {
+		            document.getElementById(this.name).style.display = 'flex';
+		            this.classList.remove("field_normal");
+			        this.classList.add("field_error");
 		            valed = false;
 				}
-				    if (document.forms.myform.lastname.value == "") {
-			            document.getElementById('pop-up2').style.display = 'flex';
-			            document.forms.myform[1].classList.remove("field_normal");
-			            document.forms.myform[1].classList.add("field_error");
-			            valed = false;
-					}                
-						if (document.forms.myform.birthdate.value == "") {
-				            document.getElementById('pop-up3').style.display = 'flex';
-				            document.forms.myform[2].classList.remove("field_normal");
-				            document.forms.myform[2].classList.add("field_error");
-				            valed = false;
-					    }   
 		    return valed;
 		}
